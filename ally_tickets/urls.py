2c9a884792a30b0e29cf40c3705c37322a545370
Gilson Reis <gilsonc.reis@gmail.com>
2019-10-01 12:15:08 -0300

    heroku deploy

--- ally_tickets/urls.py
+++ ally_tickets/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -33,4 +35,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
     url(r'^retorno/pagseguro/', include('pagseguro.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
